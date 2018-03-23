d049de2ea7e44abffad28c65dabda4f35331a17f
Gokberk Karaca <gskaraca1905@gmail.com>
2018-03-23 20:42:13 +0300

    Remove prints

--- server.c
+++ server.c
@@ -8,7 +8,7 @@
 #define MAX_KEYWORD (128)
 #define MAX_SHM_NAME (128)
 #define MAX_SEM_PREFIX (128)
-#define BUFSIZE (100)
+#define BUFFSIZE (100)
 
 int main(int argc, char **argv) {
 
@@ -24,10 +24,6 @@ int main(int argc, char **argv) {
   strcpy(shm_name, argv[1]);
   strcpy(input_file_name, argv[2]);
   strcpy(sem_prefix, argv[3]);
-
-  printf("%s", shm_name);
-  printf("%s", input_file_name);
-  printf("%s", sem_prefix);
 
   exit(0);
 }
